f9624540761be875970be2ff85ea381aa34cdf80
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-07-26 02:37:33 +0530

    final touches remain

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,9 +1,28 @@
+"use client";
 import React from "react";
+import { Button } from "../ui/button";
+import { usePathname, useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+
   return (
-    <div className=" mt-6 flex items-center justify-center">
-      <div className="">Navbar</div>
+    <div className=" mt-4 flex items-center justify-center">
+      <div className="z-10 flex gap-6 rounded-3xl bg-white p-2">
+        <Button
+          className={`rounded-3xl ${pathName === "/" ? "bg-black" : "bg-white text-black"} `}
+          onClick={() => router.push("/")}
+        >
+          Dashboard
+        </Button>
+        <Button
+          className={`rounded-3xl ${pathName === "/claims" ? "bg-black" : "bg-white text-black"} `}
+          onClick={() => router.push("/claims")}
+        >
+          Claims
+        </Button>
+      </div>
     </div>
   );
 };
