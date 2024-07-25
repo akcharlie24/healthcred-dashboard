1467efbae614c6126fec3c0a37e84296a4b3f90b
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-07-26 03:28:25 +0530

    prod ready

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -4,7 +4,6 @@ import axios from "axios";
 export const fetchHospitalData = async () => {
   try {
     const { data } = await axios({
-      // TODO: change to env
       baseURL: process.env.NEXT_PUBLIC_HEALTH_CRED_URL,
       url: "/",
     });
